9b5175baa66baa955f153c5e11a2bb940d4f3f73
Dusan Klinec <dusan.klinec@gmail.com>
2018-03-26 04:43:33 +0200

    initial CAST implementation added

---
+++ streams/block/ciphers/cast/cast.h
@@ -0,0 +1,46 @@
+#pragma once
+
+/**
+ * Sources:
+ * https://github.com/openssl/openssl/blob/master/crypto/cast/
+ * https://en.wikipedia.org/wiki/CAST-128
+ */
+
+#include <stdexcept>
+#include <cstddef>
+#include <cstdint>
+
+#include "../../block_cipher.h"
+
+namespace block {
+namespace cast {
+
+# ifdef  __cplusplus
+extern "C" {
+# endif
+
+# define CAST_ENCRYPT    1
+# define CAST_DECRYPT    0
+
+# define CAST_LONG unsigned int
+
+# define CAST_BLOCK      8
+# define CAST_KEY_LENGTH 16
+
+typedef struct cast_key_st {
+    CAST_LONG data[32];
+    int short_key;              /* Use reduced rounds for short key */
+} CAST_KEY;
+
+void CAST_set_key(CAST_KEY *key, int len, const unsigned char *data);
+void CAST_ecb_encrypt(const unsigned char *in, unsigned char *out,
+                      const CAST_KEY *key, int enc);
+void CAST_encrypt(CAST_LONG *data, const CAST_KEY *key);
+void CAST_decrypt(CAST_LONG *data, const CAST_KEY *key);
+
+# ifdef  __cplusplus
+}
+# endif
+
+}}
+
