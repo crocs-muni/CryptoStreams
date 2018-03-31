6bc290427b0bb9d1aad96cf457ae62b77e1a5960
Tamas <445653@mail.muni.cz>
2018-04-01 01:04:59 +0200

    test vectors cleaned and build directories deleted

--- cmake-build-debug/eacirc-core/external/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tom/Desktop/eacirc/eacirc-streams/eacirc-core/external/googletest
-# Build directory: /home/tom/Desktop/eacirc/eacirc-streams/cmake-build-debug/eacirc-core/external/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
