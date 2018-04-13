031e5bd577284eddc0054e81011369850241d918
Karel Kubicek <karel.kubicek@mail.muni.cz>
2018-04-13 13:37:11 +0200

    Removed CAESAR because of license
    Added empty script for future download and init of CAESAR, when needed.

--- streams/caesar/aead/ae/aeadaes128ocbtaglen96v1/aeadaes128ocbtaglen96v1_api.h
+++
@@ -1,8 +0,0 @@
-namespace Aeadaes128ocbtaglen96v1_raw {
-
-#define CRYPTO_KEYBYTES 16
-#define CRYPTO_NSECBYTES 0
-#define CRYPTO_NPUBBYTES 12
-#define CRYPTO_ABYTES 12
-
-} // namespace Aeadaes128ocbtaglen96v1_raw
--- streams/caesar/aead/hs1-siv/hs1sivlov1/hs1sivlov1_api.h
+++
@@ -1,8 +0,0 @@
-namespace Hs1sivlov1_raw {
-
-#define CRYPTO_KEYBYTES 16
-#define CRYPTO_NSECBYTES 0
-#define CRYPTO_NPUBBYTES 12
-#define CRYPTO_ABYTES 8
-
-} // namespace Hs1sivlov1_raw
