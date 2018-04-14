ddee87ed570435b586f0f94e69ed43eea502eede
Karel Kubicek <karel.kubicek@mail.muni.cz>
2018-04-14 14:13:36 +0200

    Fix gcc endiannes check

--- streams/hash/sha3/hash_functions/Keccak/brg_endian.h
+++ streams/hash/sha3/hash_functions/Keccak/brg_endian.h
@@ -10,15 +10,17 @@
 extern "C" {
 #endif
 
-#if defined(__BYTE_ORDER) && __BYTE_ORDER == __BIG_ENDIAN || defined(__BIG_ENDIAN__) ||            \
-    defined(__ARMEB__) || defined(__THUMBEB__) || defined(__AARCH64EB__) || defined(_MIBSEB) ||    \
-    defined(__MIBSEB) || defined(__MIBSEB__)
+#if defined(__BYTE_ORDER) && __BYTE_ORDER == __BIG_ENDIAN ||                                       \
+    defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__ ||                           \
+    defined(__BIG_ENDIAN__) || defined(__ARMEB__) || defined(__THUMBEB__) ||                       \
+    defined(__AARCH64EB__) || defined(_MIBSEB) || defined(__MIBSEB) || defined(__MIBSEB__)
 // It's a big-endian target architecture
 #define PLATFORM_BYTE_ORDER IS_BIG_ENDIAN
 
-#elif defined(__BYTE_ORDER) && __BYTE_ORDER == __LITTLE_ENDIAN || defined(__LITTLE_ENDIAN__) ||    \
-    defined(__ARMEL__) || defined(__THUMBEL__) || defined(__AARCH64EL__) || defined(_MIPSEL) ||    \
-    defined(__MIPSEL) || defined(__MIPSEL__)
+#elif defined(__BYTE_ORDER) && __BYTE_ORDER == __LITTLE_ENDIAN ||                                  \
+    defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__ ||                        \
+    defined(__LITTLE_ENDIAN__) || defined(__ARMEL__) || defined(__THUMBEL__) ||                    \
+    defined(__AARCH64EL__) || defined(_MIPSEL) || defined(__MIPSEL) || defined(__MIPSEL__)
 // It's a little-endian target architecture
 #define PLATFORM_BYTE_ORDER IS_LITTLE_ENDIAN
 
