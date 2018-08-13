ca7d05c944feb31aeda2074e4cf1a8820d1b3f19
Karel Kubicek <Bender250@users.noreply.github.com>
2018-08-13 09:51:28 +0200

    Fix hash bitsize (#93)
    * Fix repeating access to "source" in repeating_stream
    
    * Fix repeated division by 8 from bitsize to bytesize #90

--- streams/hash/others/hash_functions/sha2/sha256_factory.cpp
+++ streams/hash/others/hash_functions/sha2/sha256_factory.cpp
@@ -20,7 +20,7 @@ namespace others{
     int sha256_factory::Hash(int hash_bitsize, const hash::BitSequence *data, hash::DataLength data_bitsize,
                              hash::BitSequence *hash) {
         Init(hash_bitsize);
-        Update(data,data_bitsize/8);
+        Update(data,data_bitsize);
         Final(hash);
         return 0;
     }
