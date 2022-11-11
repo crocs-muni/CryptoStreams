8e8556038d10553c3bd9cae70d2422c18e7746e3
Dusan Klinec <dusan.klinec@gmail.com>
2022-11-11 20:07:28 +0100

    fix: DES, 3DES rekeying

--- streams/block/ciphers/des/triple_des.cpp
+++ streams/block/ciphers/des/triple_des.cpp
@@ -3,6 +3,7 @@
 
 #include <algorithm>
 #include <stdexcept>
+#include <memory>
 
 namespace block {
 bool triple_des_next_bit(const std::uint8_t* key, const unsigned i) {
@@ -14,6 +15,8 @@ void triple_des::keysetup(const std::uint8_t* key, const std::uint64_t keysize) 
     if (keysize != 21) {
         throw std::runtime_error("3-DES keysize has to be 21 B long.");
     }
+
+    memset(_ctx.key, 0, sizeof(uint8_t) * 24);
     for (unsigned i = 0; i < 21 * 8; ++i) {
         const unsigned byte_pos = 23 - (i / 7);
         _ctx.key[byte_pos] |= triple_des_next_bit(key, i) << ((i % 7) + 1);
